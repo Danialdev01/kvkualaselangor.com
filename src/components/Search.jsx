c31d0cd97790381b577db3917f7ae75c943ff66a
Danial Irfan <developersdani@gmail.com>
2023-06-07 15:57:41 +0800

    blog feature and basic nav

---
+++ src/components/Search.jsx
@@ -0,0 +1,72 @@
+import Fuse from "fuse.js";
+import { useState } from "react";
+
+const options = {
+  keys: ["frontmatter.title", "frontmatter.description", "frontmatter.slug"],
+  includeMatches: true,
+  minMatchCharLength: 2,
+  threshold: 0.5,
+};
+
+function Search({ searchList }) {
+  // User's input
+  const [query, setQuery] = useState("");
+
+  const fuse = new Fuse(searchList, options);
+
+  // Set a limit to the posts: 5
+  const posts = fuse
+    .search(query)
+    .map((result) => result.item)
+    .slice(0, 5);
+
+  function handleOnSearch({ target = {} }) {
+    const { value } = target;
+    setQuery(value);
+  }
+
+  return (
+    <div className="">
+      <div className="">
+        <input
+          type="text"
+          id="search"
+          value={query}
+          onChange={handleOnSearch}
+          className="block w-26 pl-10 text-sm 
+                                h-8
+                                text-gray-900 
+                               border border-gray-300
+                               rounded-lg bg-gray-50
+
+                               focus:outline-none
+                               focus:ring-blue-500
+                               focus:border-blue-500"
+          placeholder="Search Aktiviti"
+        />
+      </div>
+
+      <div className="query absolute bg-gray-800">
+        {query.length > 1 && (
+          <div className="my-3 text-white p-2">
+            Jumpa {posts.length} {posts.length === 1 ? "jawapan" : "jawapan"} untuk '
+            {query}'
+          </div>
+        )}
+
+        <ul className="list-none">
+          {posts &&
+            posts.map((post) => (
+              <li className="py-2 p-2">
+                <a className="text-lg text-blue-700 hover:text-blue-900 hover:underline underline-offset-2" href={`/aktiviti/${post.frontmatter.slug}`}>{post.frontmatter.title}</a>
+                {/* <p className="text-sm text-gray-800">{post.frontmatter.description}</p> */}
+              </li>
+            ))}
+        </ul>
+      </div>
+
+    </div>
+  );
+}
+
+export default Search;
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Kolej Vokasional Kuala Selangor';
+export const SITE_DESCRIPTION = 'Laman Sesawang Rasmi KVKS';
