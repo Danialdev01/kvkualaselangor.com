749ea4504bc7f74d072b6d700509c3bbcaffea8f
Danial Irfan <developersdani@gmail.com>
2023-06-09 15:40:37 +0800

    finish blog function

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -28,26 +28,18 @@ function Search({ searchList }) {
   return (
     <div className="">
       <div className="">
+        {/* input button */}
         <input
           type="text"
           id="search"
           value={query}
           onChange={handleOnSearch}
-          className="block w-21px text-sm 
-                                h-6
-                                text-gray-900 
-                               border border-gray-300
-                               bg-gray-50
-                                mt-2
-                                px-2
-                                rounded-sm
-                               focus:outline-none
-                               focus:ring-blue-500
-                               focus:border-blue-500"
+          className="block w-21px text-sm h-6 text-gray-900 border border-gray-300 bg-gray-50 mt-2 px-2 rounded-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
           placeholder="Search Aktiviti"
         />
       </div>
 
+      {/* prompt search untuk bagitahu user dah dapat berapa result */}
       <div className="query absolute bg-gray-800">
         {query.length > 1 && (
           <div className="my-3 text-white p-2">
@@ -56,11 +48,12 @@ function Search({ searchList }) {
           </div>
         )}
 
+          {/* list semua result */}
         <ul className="list-none">
           {posts &&
             posts.map((post) => (
-              <li className="py-2 p-2">
-                - <a className="text-lg text-blue-700 hover:text-blue-900 hover:underline underline-offset-2" href={`/aktiviti/${post.frontmatter.slug}`}>{post.frontmatter.title}</a>
+              <li className="py-2 p-2 flex">
+                <a className="text-lg text-blue-700 hover:text-blue-900 hover:underline underline-offset-2" href={`/aktiviti/${post.frontmatter.slug}`}>{post.frontmatter.title}</a>
                 {/* <p className="text-sm text-gray-800">{post.frontmatter.description}</p> */}
               </li>
             ))}
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// const untuk website 
+// supaya senang untuk edit 
 
 export const SITE_TITLE = 'Kolej Vokasional Kuala Selangor';
 export const SITE_DESCRIPTION = 'Laman Sesawang Rasmi KVKS';
