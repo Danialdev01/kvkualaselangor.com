f0560a649a6d13751a04740ca12ef41b08b324c5
Danial Irfan <developersdani@gmail.com>
2025-03-10 08:17:17 +0800

    Update page-warga.php

--- page-warga.php
+++ page-warga.php
@@ -4,6 +4,10 @@
 
     <center>
         <h3 class="text-3xl font-bold text-black">Warga</h3>
+
+        <?php get_template_part('components/aktiviti'); ?>
+        <br><br><br>
+
     </center>
 
     <?php get_template_part('includes/section', 'content');?>
